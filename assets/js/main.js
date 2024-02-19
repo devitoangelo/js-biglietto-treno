c05bc2504285b584cb83533f82922ed6200cd8da
devitoangelo <angelodevito4@gmail.com>
2024-02-19 16:27:47 +0100

    introduction

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+//Chiediamo all'utente il numero di chilometri che vuole percorrere
+
+
+const Number = prompt  ( "Numero chilometri")
+//Chiediamo all'utente l'età e salviamo 
+const age = prompt  ("Inserisci la tua età")
+
+
+console.log(Number,age)
+
+
+
+
+
+
+
+
+
+
+
